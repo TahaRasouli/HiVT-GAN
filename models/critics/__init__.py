cb7fbcdc959a160358266e19a87c42daa5986343
TahaRasouli <149691138+TahaRasouli@users.noreply.github.com>
2025-12-24 23:31:44 +0100

    Fixed a bug!

--- models/critics/__init__.py
+++ models/critics/__init__.py
@@ -1,6 +1,6 @@
 from models.critics.short_scale_critic import ShortScaleCritic
 from models.critics..mid_scale_critic import MidScaleCritic
-from models.critics..long_scale_critic import LongScaleCritic
+from models.critics.long_scale_critic import LongScaleCritic
 
 __all__ = [
     "ShortScaleCritic",
