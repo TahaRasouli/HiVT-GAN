01d8748a028d3c4269aefb027b9af38c8cfe16b3
TahaRasouli <149691138+TahaRasouli@users.noreply.github.com>
2025-12-25 01:31:24 +0100

    Fixed a bug!

--- metrics/mr.py
+++ metrics/mr.py
@@ -1,26 +1,15 @@
-from typing import Any, Callable, Optional
-
 import torch
 from torchmetrics import Metric
 
 
 class MR(Metric):
-
-    def __init__(self,
-                 miss_threshold: float = 2.0,
-                 compute_on_step: bool = True,
-                 dist_sync_on_step: bool = False,
-                 process_group: Optional[Any] = None,
-                 dist_sync_fn: Callable = None) -> None:
-        super(MR, self).__init__(compute_on_step=compute_on_step, dist_sync_on_step=dist_sync_on_step,
-                                 process_group=process_group, dist_sync_fn=dist_sync_fn)
-        self.add_state('sum', default=torch.tensor(0.0), dist_reduce_fx='sum')
-        self.add_state('count', default=torch.tensor(0), dist_reduce_fx='sum')
+    def __init__(self, miss_threshold: float = 2.0):
+        super().__init__()
         self.miss_threshold = miss_threshold
+        self.add_state("sum", default=torch.tensor(0.0), dist_reduce_fx="sum")
+        self.add_state("count", default=torch.tensor(0), dist_reduce_fx="sum")
 
-    def update(self,
-               pred: torch.Tensor,
-               target: torch.Tensor) -> None:
+    def update(self, pred: torch.Tensor, target: torch.Tensor) -> None:
         self.sum += (torch.norm(pred[:, -1] - target[:, -1], p=2, dim=-1) > self.miss_threshold).sum()
         self.count += pred.size(0)
 
