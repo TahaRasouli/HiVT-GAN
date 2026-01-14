fb85baa50b2713dd36aefefb14eb39bdf0b87d6a
TahaRasouli <149691138+TahaRasouli@users.noreply.github.com>
2026-01-14 15:46:57 +0100

    Update the visual

--- visualize_attention.py
+++ visualize_attention.py
@@ -100,6 +100,7 @@ def plot_map_only(trajectory, save_name, gt_text, pred_text, map_feats):
     ax.scatter(traj[-1, 0], traj[-1, 1], color='#d62728', s=250, edgecolors='black', zorder=11)
 
     # --- MANUAL LEGEND ---
+    # This guarantees the legend appears even if the loop data is weird
     legend_elements = [
         Line2D([0], [0], color='#1f77b4', lw=4, label='Predicted Path'),
         Line2D([0], [0], marker='o', color='w', markerfacecolor='#2ca02c', markersize=15, markeredgecolor='k', label='Start'),
