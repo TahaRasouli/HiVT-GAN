be839c75b51be75b25a00d1febcb08ea20a84eb2
TahaRasouli <149691138+TahaRasouli@users.noreply.github.com>
2026-01-14 14:56:36 +0100

    Update the visual and captioner

--- visualize_attention.py
+++ visualize_attention.py
@@ -14,7 +14,8 @@ except ImportError:
     sys.exit(1)
 
 # --- CONFIGURATION ---
-CKPT_PATH = "/mount/arbeitsdaten/studenten4/rasoulta/HiVT-GAN/lightning_logs/version_54/checkpoints/epoch=29-step=8040.ckpt"BACKBONE_PATH = "/mount/studenten/projects/rasoulta/checkpoints/vae-gan-baseline/checkpoints/epoch=45-step=60812.ckpt"
+CKPT_PATH = "/mount/arbeitsdaten/studenten4/rasoulta/HiVT-GAN/lightning_logs/version_54/checkpoints/epoch=29-step=8040.ckpt"
+BACKBONE_PATH = "/mount/studenten/projects/rasoulta/checkpoints/vae-gan-baseline/checkpoints/epoch=45-step=60812.ckpt"
 DATA_ROOT = "/mount/studenten/projects/rasoulta/dataset"
 NUSCENES_MAP_ROOT = "/mount/arbeitsdaten/analysis/rasoulta/nuscenes/nuscenes_meta/maps" # Path to folder containing 'boston-seaport.json', etc.
 
