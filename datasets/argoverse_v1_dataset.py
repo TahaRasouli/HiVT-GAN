a3b516cc24b791efdc02097740341e5d4b8624ad
TahaRasouli <149691138+TahaRasouli@users.noreply.github.com>
2026-02-04 15:21:18 +0100

    Fixed a bug!

--- datasets/argoverse_v1_dataset.py
+++ datasets/argoverse_v1_dataset.py
@@ -8,6 +8,8 @@ import torch
 from argoverse.map_representation.map_api import ArgoverseMap
 from torch_geometric.data import Data, Dataset
 from tqdm import tqdm
+from argparse import ArgumentParser
+
 
 # Fallback for TemporalData if not defined in a local utils.py
 try:
