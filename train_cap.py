040788938b6bcfa31d4dbf9714ed09ad951fc4f6
TahaRasouli <149691138+TahaRasouli@users.noreply.github.com>
2026-02-09 11:42:50 +0100

    Fixing a bug!

--- train_cap.py
+++ train_cap.py
@@ -61,7 +61,7 @@ def main():
         root=args.root,
         train_batch_size=args.batch_size,
         val_batch_size=args.batch_size,
-        shuffle=True,
+        # shuffle=True,
         num_workers=args.num_workers,
         pin_memory=args.pin_memory,
         persistent_workers=args.persistent_workers,
